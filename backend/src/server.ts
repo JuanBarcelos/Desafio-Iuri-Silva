32b15d97fc761ecefb833a0bbb9cd056ee988dd2
JuanBarcelos <juanbarcelos@outlook.com.br>
2022-01-19 10:33:35 -0300

    alteração no back

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,8 @@ import cors from 'cors';
 
 const app = express();
 
+const port  = process.env.PORT || 3001;
+
 
 app.use(cors({
     origin: '*'
@@ -13,4 +15,4 @@ app.use(cors({
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => console.log("Server is Running..."));
+app.listen(port, () => console.log("Server is Running..."));
